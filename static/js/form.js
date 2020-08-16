c5f4bf872304e054d6309bd5b732048f3495f4ab
trajkd <trajkd@gmail.com>
2020-08-16 16:32:28 +0200

    fix: add cors header 7

--- static/js/form.js
+++ static/js/form.js
@@ -64,6 +64,7 @@ $(document).ready(function() {
 		},
 		error: function(error) {
 			$loader.hide();
+			cconsole.log(error);
 			$messageModalContent.removeClass("alert-success").addClass("alert-danger");
 			$messageModalContent.html("Failed to send the message...")
 			$modal.modal("show");
