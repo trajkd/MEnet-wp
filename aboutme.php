a64fe433f555e7d401f7a864066358b7ac002be3
trajkd <trajkd@gmail.com>
2020-08-20 08:58:10 +0200

    fix: about ME page

--- aboutme.php
+++ aboutme.php
@@ -4,18 +4,19 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width-device-width, initial-scale-1.0">
-	<link rel="stylesheet" href="/wp-content/themes/DPAitaly-wp/static/fonts/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/boffi-libs.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/boffi.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/clean-blog.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/style.css">
-  <link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/static.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/dipaolo.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/dipaolo2.css">
-  <link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/header.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/lago.css">
-	<link rel="stylesheet" type="text/css" href="/wp-content/themes/DPAitaly-wp/static/css/lago-style.css">
-	<script src="/wp-content/themes/DPAitaly-wp/static/js/main.js"></script>
+	<link rel="stylesheet" href="/wp-content/themes/MEnet-wp/static/fonts/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/boffi-libs.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/boffi.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/static.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/dipaolo.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/dipaolo2.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/header.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/header.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/clean-blog.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/lago.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/lago-style.css">
+	<script src="/wp-content/themes/MEnet-wp/static/js/main.js"></script>
   	<script src="https://kit.fontawesome.com/4c0b3ae1d6.js" crossorigin="anonymous"></script>
 	<title>About ME - <?php bloginfo( 'name' ) ?></title>
 </head>
@@ -35,7 +36,7 @@
       </div>
     </div>
   </div>
-<script src="/wp-content/themes/DPAitaly-wp/static/js/jquery.js"></script>
-<script src="/wp-content/themes/DPAitaly-wp/static/js/sidebar.js"></script>
+<script src="/wp-content/themes/MEnet-wp/static/js/jquery.js"></script>
+<script src="/wp-content/themes/MEnet-wp/static/js/sidebar.js"></script>
 </body>
 </html>
