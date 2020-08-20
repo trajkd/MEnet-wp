623836fa82ef5d5d139bdcd359067196d9566060
trajkd <trajkd@gmail.com>
2020-08-20 09:26:42 +0200

    fix: about ME page 3

--- aboutme.php
+++ aboutme.php
@@ -35,6 +35,8 @@
       </div>
     </div>
   </div>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 <script src="/wp-content/themes/MEnet-wp/static/js/jquery.js"></script>
 <script src="/wp-content/themes/MEnet-wp/static/js/clean-blog.js"></script>
 <script src="/wp-content/themes/MEnet-wp/static/js/sidebar.js"></script>
