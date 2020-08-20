4f51b3f95a4183058a198faec8bce301803b4de3
trajkd <trajkd@gmail.com>
2020-08-20 10:26:49 +0200

    fix: about ME page 5

---
+++ wiki.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: Wiki */ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+	<link rel="stylesheet" href="/wp-content/themes/MEnet-wp/static/fonts/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/boffi-libs.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/boffi.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/static.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/dipaolo.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/dipaolo2.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/header.css">
+  <link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/header.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/clean-blog.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/lago.css">
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/MEnet-wp/static/css/lago-style.css">
+	<link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+  	<script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
+	<script src="/wp-content/themes/MEnet-wp/static/js/main.js"></script>
+  	<script src="https://kit.fontawesome.com/4c0b3ae1d6.js" crossorigin="anonymous"></script>
+	<title>About ME - <?php bloginfo( 'name' ) ?></title>
+</head>
+<body>
